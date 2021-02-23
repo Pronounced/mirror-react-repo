8bf9b0341063a8dbae2cb907743ac53738af24f7
Pronounced <jokerknight670@gmail.com>
2021-02-23 09:56:59 -0600

    Added Display of post date using Moment.

--- ui/src/components/Feed.js
+++ ui/src/components/Feed.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import samplePosts from '../sampleData'
+import moment, { relativeTimeRounding } from 'moment'
+
 const Feed = (props) => (
   <div className="feed">
     <ul>
       {samplePosts.map(post => (
         <li className="feed-list-item"> 
           <div className="feed-list-item-title" onClick={props.handleClick}>{post.title}</div>
-          <div className="feed-list-item-byline"><span className="feed-list-item-byline-author">{post.author}</span></div>
+        <div className="feed-list-item-byline"><span className="feed-list-item-byline-author">{post.author}</span> <span>{moment(post.createdAt).fromNow()}</span></div>
           <img alt='' src={post.imageUrl} onClick={props.handleClick} className="feed-list-item-image"/>
           <span className="feed-list-item-lede">{post.body}</span>
         </li>
@@ -16,3 +18,4 @@ const Feed = (props) => (
 );
 
 export default Feed;
+
